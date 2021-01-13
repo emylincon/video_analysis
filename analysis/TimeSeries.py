e183157f073eca937789f64161f1f2e7f1e7a790
Emeka Ugwuanyi <emylincon@gmail.com>
2021-01-13 11:55:06 +0000

    refactor code

--- analysis/TimeSeries.py
+++ analysis/TimeSeries.py
@@ -8,8 +8,8 @@ class TimeSeriesPredict:
             self.model = keras.models.load_model('../notebooks/lstm_model.h5')
         else:
             self.model = keras.models.load_model('notebooks/lstm_model.h5')
-        self.shape = (1,10,1)
-        self.last_input = np.array([[1,0,1] + [0]*7]).reshape(1,10,1)
+        self.shape = (1, 10, 1)
+        self.last_input = np.array([[1, 0, 1] + [0] * 7]).reshape(1, 10, 1)
 
     def data_prepare(self, my_input):
         return np.array([my_input]).reshape(*self.shape)
@@ -18,7 +18,6 @@ class TimeSeriesPredict:
         raw_prediction = self.model.predict(self.data_prepare(last_input))
         prediction = list(raw_prediction[0])
         return [round(i) for i in prediction]
-
 
 # a = TimeSeriesPredict().get_output([0] * 10)
 # print(a)
