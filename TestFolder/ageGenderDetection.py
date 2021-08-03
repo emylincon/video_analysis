3a1f922b41d114dc7eedfa5ec1c462ed4801013d
emylincon <emylincon@gmail.com>
2021-08-03 15:16:50 +0100

    refactored

--- TestFolder/ageGenderDetection.py
+++ TestFolder/ageGenderDetection.py
@@ -1,5 +1,4 @@
 import cv2 as cv
-import math
 import time
 
 cap = cv.VideoCapture(0)
@@ -22,17 +21,18 @@ def getFaceBox(net, frame, conf_threshold=0.7):
             x2 = int(detections[0, 0, i, 5] * frameWidth)
             y2 = int(detections[0, 0, i, 6] * frameHeight)
             bboxes.append([x1, y1, x2, y2])
-            cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
+            cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight / 150)), 8)
     return frameOpencvDnn, bboxes
 
-faceProto = "data/modelNweight/modelNweight/opencv_face_detector.pbtxt"
-faceModel = "data/modelNweight/modelNweight/opencv_face_detector_uint8.pb"
 
-ageProto = "data/modelNweight/modelNweight/age_deploy.prototxt"
-ageModel = "data/modelNweight/modelNweight/age_net.caffemodel"
+faceProto = "../data/modelNweight/modelNweight/opencv_face_detector.pbtxt"
+faceModel = "../data/modelNweight/modelNweight/opencv_face_detector_uint8.pb"
 
-genderProto = "data/modelNweight/modelNweight/gender_deploy.prototxt"
-genderModel = "data/modelNweight/modelNweight/gender_net.caffemodel"
+ageProto = "../data/modelNweight/modelNweight/age_deploy.prototxt"
+ageModel = "../data/modelNweight/modelNweight/age_net.caffemodel"
+
+genderProto = "../data/modelNweight/modelNweight/gender_deploy.prototxt"
+genderModel = "../data/modelNweight/modelNweight/gender_net.caffemodel"
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
@@ -52,7 +52,8 @@ def age_gender_detector(frame):
     frameFace, bboxes = getFaceBox(faceNet, frame)
     for bbox in bboxes:
         # print(bbox)
-        face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
+        face = frame[max(0, bbox[1] - padding):min(bbox[3] + padding, frame.shape[0] - 1),
+               max(0, bbox[0] - padding):min(bbox[2] + padding, frame.shape[1] - 1)]
 
         blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
         genderNet.setInput(blob)
@@ -63,23 +64,23 @@ def age_gender_detector(frame):
         age = ageList[agePreds[0].argmax()]
 
         label = "{},{}".format(gender, age)
-        cv.putText(frameFace, label, (bbox[0], bbox[1]-10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv.LINE_AA)
+        cv.putText(frameFace, label, (bbox[0], bbox[1] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2,
+                   cv.LINE_AA)
     return frameFace
 
 
 while True:
-    ret, frame = cap.read()
+    ret, frame_obj = cap.read()
 
-    #frame = cv.resize(frame, (0,0), fx=0.5, fy=0.5)
-    cv.imshow("Original", frame)
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # frame = cv.resize(frame, (0,0), fx=0.5, fy=0.5)
+    cv.imshow("Original", frame_obj)
+    gray = cv.cvtColor(frame_obj, cv.COLOR_BGR2GRAY)
     cv.imshow("Grey", gray)
-    cv.imshow("Detected", age_gender_detector(frame))
+    cv.imshow("Detected", age_gender_detector(frame_obj))
 
     ch = cv.waitKey(1)
     if ch & 0xFF == ord('q'):
         break
-
 
 cap.release()
 cv.destroyAllWindows()
